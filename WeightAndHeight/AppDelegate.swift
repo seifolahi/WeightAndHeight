bff879922e6ef3c2a3e1426c11a619c52c52a162
Hamid Reza Seifolahi <hr.seifolahi@gmail.com>
2021-12-10 02:53:30 +0400

    wheel rotation and human figure

---
+++ WeightAndHeight/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  WeightAndHeight
+//
+//  Created by Hamid reza Seifolahi on 12/7/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
